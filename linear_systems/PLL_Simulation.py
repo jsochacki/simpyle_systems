2514c1ed49256be1e88fc3f73f2773a72f94cbf2
ThomasMurphyViasat <Thomas.Murphy@viasat.com>
2017-06-23 10:58:03 -0700

    Added Noise for Loop Filter (#3)

--- linear_systems/PLL_Simulation.py
+++ linear_systems/PLL_Simulation.py
@@ -19,6 +19,7 @@ Add phase noise plot if possible?
 from scipy import signal
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 # CP, VCO, and Divider parameters
 kpd = 2.5e-3         # icp also
@@ -66,18 +67,6 @@ def graph_phase(w, phase, title): # function for graphing phase response
     plt.xlabel(r'Frequency (Hz)')
     plt.ylabel(r'Phase (deg)')
 
-def main(): # function to create all 3 response graphs
-    # transfer response of loop filter
-    graph_mag(freq, mag, 'Loop Filter Response') 
-    graph_phase(freq, phase, 'Loop Filter Response')
-    # Open loop transfer response for PLL
-    graph_mag(freq, mag_o, 'Open Loop PLL Response') 
-    graph_phase(freq, phase_o, 'Open Loop PLL Response')
-    # Closed loop transfer response for PLL
-    graph_mag(freq, mag_c, 'Closed Loop PLL Response') 
-    graph_phase(freq, phase_c, 'Closed Loop PLL Response')
-    plt.show()
-
 def graph_cl(): # graph just closed loop gain response
     # Closed loop transfer response for PLL
     graph_mag(freq, mag_c, 'Closed Loop PLL Response') 
@@ -98,16 +87,25 @@ def graph_lf(): # graph just open loop gain response
 
 def graph_noise(): # graph just phase noise
     plt.figure()
-    plt.semilogx(freq, s_ref, 'r--')
-    plt.semilogx(freq, s_vco, 'g--')
-    plt.semilogx(freq, mag_c)                 # total phase noise plot
+    plt.semilogx(freq, vco_noise, 'b--')
+    plt.semilogx(freq, ref_noise, 'g--')
+    plt.semilogx(freq, s_ref, 'm')
+    plt.semilogx(freq, vco_noise, 'r')
+#    plt.semilogx(freq, r2_noise, 'r--')
+#    plt.semilogx(freq, r3_noise, 'g--')
+#    plt.semilogx(freq, r4_noise, 'b--')
+#    plt.semilogx(freq, r_tot, 'm--')
     plt.grid()
     plt.ylim([-160,-60])
     plt.gca().xaxis.grid(True, which='minor')
     plt.title('Phase Noise')
     plt.xlabel(r'Frequency (Hz)')
-    plt.ylabel(r'Phase Noise (dbc/Hz)')
+    plt.ylabel(r'Phase Noise Power (dbc/Hz)')
     plt.show()
+
+#def r_noise(vx, tr): # equation to convert resistor noise from V to dBc/Hz
+#    rout = (vx * kvco) / (freq * math.sqrt(2)) * abs(tr / [1 + mag_o/n])
+#    return rout
 
 # numerators for transient response
 num_filter = [t2, 1]
@@ -132,29 +130,29 @@ w_o, mag_o, phase_o = signal.bode(f_open, x_new)    # open loop creation
 w_c, mag_c, phase_c = signal.bode(f_closed, x_new)  # closed loop creation
 
 
-
-
 # Phase Noise Simulation
 ref_noise = np.piecewise(freq, [freq < 1, (10 > freq) & (freq >= 1), (100 > freq) & (freq >= 10), (1e3 > freq) & (freq >= 100), (1e4 > freq) & (freq >= 1e3), freq >= 1e4], [lambda x: -32 * np.log10(1241 * x), lambda x: -21 * np.log10(5.179e4 * x), lambda x: -17 * np.log10(1.145e6 * x), lambda x: -3 * np.log10(4.642e43 * x), lambda x: -5 * np.log10(1e25 * x), -145])
 vco_noise = np.piecewise(freq, [freq < 1e4, (1e5 > freq) & (freq >= 1e4), (1e6 > freq) & (freq >= 1e5), (1e7 > freq) & (freq >= 1e6), (2e7 > freq) & (freq >= 1e7), (1e8 > freq) & (freq >= 2e7), freq >= 1e8], [lambda x: -30 * np.log10(0.1166 * x), lambda x: -23 * np.log10(x), lambda x: -21 * np.log10(2.994 * x), lambda x: -20 * np.log10(6.310 * x), lambda x: -19.93 * np.log10(6.711 * x), lambda x: 42.73 * 10**(-3.862e-8 * x) - 169.2, -196])
+s_ref = 10*np.log10(mag_c)
+s_vco = vco_noise * 1/np.asarray(mag_o+1)
+
+# loop filter noise
+v2 = math.sqrt(4 * 300 * 1.380658e-23 * r2)
+v3 = math.sqrt(4 * 300 * 1.380658e-23 * r3)
+v4 = math.sqrt(4 * 300 * 1.380658e-23 * r4)
 z1 = signal.TransferFunction([c2*r2, 1], [c1*c2*r2, c1+c2, 0])
 z2 = signal.TransferFunction([c3*c4*r3*r4, (c3*r3)+(c4*r4)+(r3*c4), 1],[c3*c4*r4, c3+c4, 0])
 tmid = signal.TransferFunction(1, [c3*c4*r3*r4, (c3*r3)+(c4*r4)+(r3*c4), 1])
-v1, c1, z1= signal.bode(z1, x_new)
-v2, c2, z2= signal.bode(z2, x_new)
-v3, c3, z3= signal.bode(tmid, x_new)
-tr2 = signal.TransferFunctio()
-tr3 = (c3*c2)/(c1+c2)]
-tr4 = signal.TransferFunctio()
-s_ref = mag_c + 10*np.log10(10**(ref_noise/10)) #not working
-s_vco = mag_c + 10*np.log10(10**(vco_noise/10)) #not working
-
-
-
-
-
-
-
+vn1, carry1, zn1= signal.bode(z1, x_new)
+vn2, carry2, zn2= signal.bode(z2, x_new)
+vn3, carry3, zn3= signal.bode(tmid, x_new)
+tr2 = (carry3 * x_new * c2 * carry2) / (1 + x_new * [c2*r2 + c1*carry2 + c2*carry2] + x_new**2*c1*c2*r2*carry2)
+tr3 = (carry3*carry2)/(carry1+carry2)
+tr4 = (1 + x_new*c3*(r3+carry1)) / (1 + x_new*[(c3+c4)*(r3+carry1)+c4*r4] + x_new**2 * c3 * c4 * r4 * (r3+carry1))
+r2_noise = np.transpose(r_noise(v2, tr2))
+r3_noise = np.transpose(r_noise(v3, tr3))
+r4_noise = np.transpose(r_noise(v4, tr4))
+r_tot = 10*np.log10(10**(r2_noise/10) + 10**(r3_noise/10) + 10**(r4_noise/10))
 
 
 
--- linear_systems/Test_simulation.py
+++ linear_systems/Test_simulation.py
@@ -19,13 +19,14 @@ Add phase noise plot if possible?
 from scipy import signal
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 # CP, VCO, and Divider parameters
 kpd = 5e-3
 kvco = 30e6
 n = 4500
 
-# frequency analysis data
+## frequency analysis data
 #f1 = 5.8e9 * 2 * np.pi # starting frequency
 #f2 = 5.913e9 * 2 * np.pi # final frequency
 
@@ -49,9 +50,9 @@ t2_open = t2 * k
 
 # adjusted constants for closed loop response
 s1 = (t2 * k) / n
-s0 = k /n
+s0 = k / n
 
-# constants for accurate analysis
+## constants for accurate analysis
 #q0 = (k * (f2-f1)) / n
 #q1 = (k * (f2-f1) * t2) / n
 
@@ -75,45 +76,52 @@ def graph_phase(w, phase, title): # function for graphing phase response
 
 def main(): # function to create all 3 response graphs
     # transfer response of loop filter
-    graph_mag(x, mag, 'Loop Filter Response') 
-    graph_phase(x, phase, 'Loop Filter Response')
+    graph_mag(freq, mag, 'Loop Filter Response') 
+    graph_phase(freq, phase, 'Loop Filter Response')
     # Open loop transfer response for PLL
-    graph_mag(x, mag_o, 'Open Loop PLL Response') 
-    graph_phase(x, phase_o, 'Open Loop PLL Response')
+    graph_mag(freq, mag_o, 'Open Loop PLL Response') 
+    graph_phase(freq, phase_o, 'Open Loop PLL Response')
     # Closed loop transfer response for PLL
-    graph_mag(x, mag_c, 'Closed Loop PLL Response') 
-    graph_phase(x, phase_c, 'Closed Loop PLL Response')
+    graph_mag(freq, mag_c, 'Closed Loop PLL Response') 
+    graph_phase(freq, phase_c, 'Closed Loop PLL Response')
     plt.show()
 
 def graph_cl(): # graph just closed loop gain response
     # Closed loop transfer response for PLL
-    graph_mag(x, mag_c, 'Closed Loop PLL Response') 
-    graph_phase(x, phase_c, 'Closed Loop PLL Response')
+    graph_mag(freq, mag_c, 'Closed Loop PLL Response') 
+    graph_phase(freq, phase_c, 'Closed Loop PLL Response')
     plt.show()
 
 def graph_ol(): # graph just open loop gain response
     # Open loop transfer response for PLL
-    graph_mag(x, mag_o, 'Open Loop PLL Response') 
-    graph_phase(x, phase_o, 'Open Loop PLL Response')
+    graph_mag(freq, mag_o, 'Open Loop PLL Response') 
+    graph_phase(freq, phase_o, 'Open Loop PLL Response')
     plt.show()
 
 def graph_lf(): # graph just open loop gain response
     # Open loop transfer response for PLL
-    graph_mag(x, mag, 'Loop Filter Response') 
-    graph_phase(x, phase, 'Loop Filter Response')
+    graph_mag(freq, mag, 'Loop Filter Response') 
+    graph_phase(freq, phase, 'Loop Filter Response')
     plt.show()
 
 def graph_noise(): # graph just phase noise
     plt.figure()
-    plt.semilogx(x, tot)    # Bode magnitude plot
-    plt.semilogx(x, losc, 'r')
+    plt.semilogx(freq, r3_noise, 'r')    # Bode magnitude plot
+    plt.semilogx(freq, losc, 'g')
+    plt.semilogx(freq, tot, 'b')
     plt.grid()
-    plt.ylim([-200,-50])
-    plt.xlim([1e3,1e6])
+    plt.ylim([-160,-60])
+    #plt.xlim([1e3,1e7])
     plt.gca().xaxis.grid(True, which='minor')
     plt.title('Phase Noise')
     plt.xlabel(r'Frequency (Hz)')
     plt.ylabel(r'Phase Noise Power (dbc/Hz)')
+    plt.show()
+
+def r_noise(vx, tr): # equation to convert resistor noise from V to dBc/Hz
+    rout = 20*np.log10((vx * kvco) / (freq * math.sqrt(2)) * abs(tr / [1 + mag_o / n]))
+#    rout = (vx * tr * kvco) / (freq * math.sqrt(2)) *abs(1/(1+mag_o))
+    return rout
 
 #def graph_acc(): # graph just open loop gain response
 #    # Open loop transfer response for PLL
@@ -139,8 +147,8 @@ f_closed = signal.TransferFunction(num_closed, den_closed)
 #f_accurate = signal.TransferFunction(num_accurate, den_closed)
 
 # create plot data
-x = np.arange (1000, 100e4, 10)                      # frequency range 0.1Hz to 100MHz
-x_new = x * 2 * np.pi                               # change frequency to rads/s for bode plot function
+freq = np.arange (100, 10e5, 10)                      # frequency range 0.1Hz to 100MHz
+x_new = freq * 2 * np.pi                               # change frequency to rads/s for bode plot function
 w, mag, phase = signal.bode(f_filter, x_new)        # loop filter creation
 w_o, mag_o, phase_o = signal.bode(f_open, x_new)    # open loop creation
 w_c, mag_c, phase_c = signal.bode(f_closed, x_new)  # closed loop creation
@@ -149,11 +157,24 @@ w_c, mag_c, phase_c = signal.bode(f_closed, x_new)  # closed loop creation
 
 
 # Phase Noise Simulation
-losc = (-100.9) - (20 * np.log10( x / 10e3))
+losc = (-100.9) - (20 * np.log10(freq/10e3))
 tot = mag_c + losc
 
 
-
+# Phase Noise Simulation
+v2 = math.sqrt(4 * 300 * 1.380658e-23 * r2)
+v3 = math.sqrt(4 * 300 * 1.380658e-23 * r3)
+v4 = math.sqrt(4 * 300 * 1.380658e-23 * r4)
+z1 = signal.TransferFunction([c2*r2, 1], [c1*c2*r2, c1+c2, 0])
+z2 = signal.TransferFunction([c3*c4*r3*r4, (c3*r3)+(c4*r4)+(r3*c4), 1],[c3*c4*r4, c3+c4, 0])
+tmid = signal.TransferFunction(1, [c3*c4*r3*r4, (c3*r3)+(c4*r4)+(r3*c4), 1])
+vn1, carry1, zn1= signal.bode(z1, x_new)
+vn2, carry2, zn2= signal.bode(z2, x_new)
+vn3, carry3, zn3= signal.bode(tmid, x_new)
+##tr2 = signal.TransferFunction([carry3*c2*carry2, 0],[c1*c2*r2*carry2, c2*r2 + c1*carry2 + c2*carry2, 1])
+tr3 = (carry3*carry2)/(carry1+carry2)
+##tr4 = signal.TransferFunction([c3*(r3*carry1), 1],[c3*c4*r4*(r3*carry1), (c3+c4)*(r3*carry1) + c4*r4, 1])
+r3_noise = np.transpose(r_noise(v3, tr3))
 
 
 
